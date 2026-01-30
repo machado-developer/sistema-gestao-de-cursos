7aec77f9c8765eab15567872d8b71099363e1876
Antonio Machado <antoniolundomachado@gmail.com>
2026-01-30 11:55:53 +0100

    fix bug useeffe

--- src/components/forms/AlunoForm.tsx
+++ src/components/forms/AlunoForm.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { useForm, Controller } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
